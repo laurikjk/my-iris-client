ff4c9d112c8f0d3e990e29b0f6a6b86c57d563e1
Martti Malmi <sirius@iki.fi>
2025-01-07 13:19:21 -0600

    fix recursive looping

--- src/pages/messages/Channels.ts
+++ src/pages/messages/Channels.ts
@@ -1,10 +1,10 @@
 import {Channel, deserializeChannelState, NostrFilter} from "nostr-double-ratchet"
-import {localState, Unsubscribe} from "irisdb"
 import {VerifiedEvent} from "nostr-tools"
 import {MessageType} from "./Message"
+import {localState} from "irisdb"
 import {ndk} from "irisdb-nostr"
 
-const channels = new Map<string, Channel>()
+const channels = new Map<string, Channel | undefined>()
 
 const subscribe = (filter: NostrFilter, onEvent: (event: VerifiedEvent) => void) => {
   const sub = ndk().subscribe(filter)
@@ -16,63 +16,52 @@ const subscribe = (filter: NostrFilter, onEvent: (event: VerifiedEvent) => void)
 }
 
 export async function getChannel(id: string): Promise<Channel | undefined> {
-  if (!channels.has(id)) {
-    const statePromise = new Promise<Channel | undefined>((resolve) => {
-      let unsub: Unsubscribe | undefined = undefined
-      unsub = localState
-        .get("channels")
-        .get(id)
-        .get("state")
-        .on(
-          (state) => {
-            console.log("channel state", state, id)
-            if (typeof state === "string" && state !== null) {
-              const deserialized = deserializeChannelState(state)
-              console.log("deserialized", deserialized)
-              const channel = new Channel(subscribe, deserialized)
-              channels.set(id, channel)
-              resolve(channel)
-            } else {
-              resolve(undefined)
-            }
-            unsub?.()
-          },
-          true,
-          2
-        )
-    })
+  if (channels.has(id)) return channels.get(id)
 
-    return await statePromise
+  // Mark as loading to prevent duplicate work
+  channels.set(id, undefined)
+
+  const state = await localState.get("channels").get(id).get("state").once()
+
+  if (typeof state === "string" && state !== null) {
+    const deserialized = deserializeChannelState(state)
+    const channel = new Channel(subscribe, deserialized)
+    channels.set(id, channel)
+    return channel
   }
-  return channels.get(id)
+
+  return undefined
 }
 
 // function that gets all our channels and subscribes to messages from them
 export function getChannels() {
-  return localState.get("channels").forEach(async (data, id) => {
-    if (channels.has(id)) return
-    if (data) {
-      console.log("got channel", id, data, typeof data)
-      id = id.split("/").pop()!
-      const channel = await getChannel(id)
-      console.log('channel', channel)
-      if (!channel?.onMessage) return
-      console.log("channel sub", channel)
-      channel.onMessage(async (msg) => {
-        console.log("received message", msg)
+  return localState
+    .get("channels")
+    .once()
+    .then(async (channelData) => {
+      for (const [id, data] of Object.entries(channelData || {})) {
+        if (channels.has(id)) continue
+        if (data) {
+          const channelId = id.split("/").pop()!
+          const channel = await getChannel(channelId)
+          if (!channel?.onMessage) continue
 
-        const message: MessageType = {
-          id: msg.id,
-          sender: id.split(":").shift()!,
-          content: msg.data,
-          time: msg.time,
+          channel.onMessage(async (msg) => {
+            console.log("received message", msg)
+
+            const message: MessageType = {
+              id: msg.id,
+              sender: id.split(":").shift()!,
+              content: msg.data,
+              time: msg.time,
+            }
+            localState.get("channels").get(id).get("messages").get(msg.id).put(message)
+            console.log("puttin mesg", msg)
+            localState.get("channels").get(id).get("latest").put(msg)
+          })
         }
-        localState.get("channels").get(id).get("messages").get(msg.id).put(message)
-        console.log("puttin mesg", msg)
-        localState.get("channels").get(id).get("latest").put(msg)
-      })
-    }
-  })
+      }
+    })
 }
 
 localState.get("user").on((u) => {
