d22387e8a877613b3a7029ec9e1f45be4afc5d46
Martti Malmi <sirius@iki.fi>
2025-02-27 11:18:09 +0200

    move react-dom/client to vendor bundle

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
         manualChunks: {
           vendor: [
             "react",
+            "react-dom/client",
             "react-router",
             "react-helmet",
             "@nostr-dev-kit/ndk",
