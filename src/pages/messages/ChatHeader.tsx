5c2e119e2a525762e2c4d88cd3117d5303a7daea
Martti Malmi <sirius@iki.fi>
2025-03-06 14:48:06 +0200

    scroll down on chat header click

--- src/pages/messages/ChatHeader.tsx
+++ src/pages/messages/ChatHeader.tsx
@@ -81,7 +81,7 @@ const ChatHeader = ({id, messages}: ChatHeaderProps) => {
     socialGraph().getFollowedByUser(user).has(myPubKey) || user === myPubKey
 
   return (
-    <MiddleHeader showNotifications={false}>
+    <MiddleHeader showNotifications={false} scrollDown={true}>
       <div className="flex items-center justify-between w-full">
         <div className="flex flex-row items-center gap-2">
           {id && <UserRow avatarWidth={32} pubKey={user} />}
