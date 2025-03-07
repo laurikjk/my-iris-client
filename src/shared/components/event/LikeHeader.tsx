e7c3c4709e4c68a3f2352b42c9ecf490cbdf630d
Martti Malmi <sirius@iki.fi>
2025-03-07 12:55:00 +0200

    likes tab headers

--- src/shared/components/event/LikeHeader.tsx
+++ src/shared/components/event/LikeHeader.tsx
@@ -1,3 +1,5 @@
+import {CustomEmojiComponent} from "../embed/nostr/CustomEmojiComponent"
+import CustomEmoji from "../embed/nostr/CustomEmoji"
 import {Name} from "@/shared/components/user/Name"
 import {NDKEvent} from "@nostr-dev-kit/ndk"
 import {Link} from "react-router"
@@ -9,14 +11,28 @@ interface LikeHeaderProps {
 
 function LikeHeader({event}: LikeHeaderProps) {
   const reactionText =
-    event.content === "+" ? "liked" : `reacted with ${event.content.slice(0, 2)}`
+    event.content === "+" ? (
+      <span className="text-base-content/50">liked</span>
+    ) : (
+      <>
+        <span className="text-base-content/50">reacted with </span>
+        {event.content.match(CustomEmoji.regex) ? (
+          <CustomEmojiComponent
+            match={event.content.match(CustomEmoji.regex)?.[1] || event.content}
+            event={event}
+          />
+        ) : (
+          <span>{event.content}</span>
+        )}
+      </>
+    )
 
   return (
     <Link
       to={`/${nip19.npubEncode(event.pubkey)}`}
-      className="flex items-center font-bold text-sm text-base-content/50 hover:underline"
+      className="flex items-center font-bold text-sm hover:underline"
     >
-      <Name pubKey={event.pubkey} />
+      <Name pubKey={event.pubkey} className="text-base-content/50" />
       <span className="mx-1">{reactionText}</span>
     </Link>
   )
