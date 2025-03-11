c7dc9bbf549637fdddf051cc9fbfadda81405b71
Martti Malmi <sirius@iki.fi>
2025-03-11 11:30:19 +0200

    audio embed

--- src/shared/components/embed/Audio.tsx
+++ src/shared/components/embed/Audio.tsx
@@ -4,7 +4,7 @@ const Audio: Embed = {
   regex: /(https?:\/\/\S+\.(?:mp3|wav|ogg|flac)(?:\?\S*)?)\b/gi,
   settingsKey: "enableAudio",
   component: ({match}) => {
-    return <audio className="my-2" src={match} controls={true} loop={true} />
+    return <audio className="my-2 mx-4" src={match} controls={true} loop={true} />
   },
 }
 
--- src/shared/components/embed/audio/AudioComponent.tsx
+++
@@ -1,9 +0,0 @@
-interface AudioComponentProps {
-  match: string
-}
-
-function AudioComponent({match}: AudioComponentProps) {
-  return <audio src={match} controls={true} loop={true} />
-}
-
-export default AudioComponent
