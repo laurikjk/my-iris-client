4ec23189cd25ab79bf604f08a13dcb249ac2b14f
Martti Malmi <sirius@iki.fi>
2025-03-12 10:32:12 +0200

    if wallet connect, wallet icon takes to settings

--- src/pages/wallet/WalletPage.tsx
+++ src/pages/wallet/WalletPage.tsx
@@ -2,9 +2,16 @@ import RightColumn from "@/shared/components/RightColumn.tsx"
 import Trending from "@/shared/components/feed/Trending.tsx"
 import {useLocalState} from "irisdb-hooks/src/useLocalState"
 import Widget from "@/shared/components/ui/Widget"
+import {useNavigate} from "react-router"
 
 export default function WalletPage() {
   const [myPubKey] = useLocalState("user/publicKey", "")
+  const [isWalletConnect] = useLocalState("user/walletConnect", false)
+  const navigate = useNavigate()
+
+  if (isWalletConnect) {
+    navigate("/settings/wallet", {replace: true})
+  }
 
   return (
     <div className="flex justify-center h-screen">
