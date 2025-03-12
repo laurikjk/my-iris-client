803a45b48250bfd3ca855aba7b38139cc2d3ced1
Martti Malmi <sirius@iki.fi>
2025-03-12 12:04:58 +0200

    wallet -> settings unless @npub.cash in profile

--- src/pages/wallet/WalletPage.tsx
+++ src/pages/wallet/WalletPage.tsx
@@ -2,15 +2,18 @@ import RightColumn from "@/shared/components/RightColumn.tsx"
 import Trending from "@/shared/components/feed/Trending.tsx"
 import {useLocalState} from "irisdb-hooks/src/useLocalState"
 import Widget from "@/shared/components/ui/Widget"
+import useProfile from "@/shared/hooks/useProfile"
 import {useNavigate} from "react-router"
 
 export default function WalletPage() {
   const [myPubKey] = useLocalState("user/publicKey", "")
+  const profile = useProfile(myPubKey)
   const [isWalletConnect] = useLocalState("user/walletConnect", false)
   const navigate = useNavigate()
 
-  if (isWalletConnect) {
+  if (isWalletConnect || (profile && !profile.lud16?.endsWith("@npub.cash"))) {
     navigate("/settings/wallet", {replace: true})
+    return null
   }
 
   return (
