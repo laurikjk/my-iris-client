c4aac5a664c3a272175b3899343d83b96953d913
Martti Malmi <sirius@iki.fi>
2025-04-13 21:44:59 +0300

    errorboundary width

--- src/shared/components/ui/ErrorBoundary.tsx
+++ src/shared/components/ui/ErrorBoundary.tsx
@@ -37,15 +37,17 @@ export default class ErrorBoundary extends React.Component<
       // Render any custom fallback UI with the error message
       return (
         <div className="p-2">
-          <button
-            onClick={() => window.location.reload()}
-            className="btn btn-primary mb-4"
-          >
-            Reload Page
-          </button>
-          <h1>Something went wrong.</h1>
-          <p>Error: {this.state.errorMessage}</p>
-          <pre className="text-xs overflow-auto mt-8">{this.state.stack}</pre>
+          <div className="max-w-md md:max-w-lg lg:max-w-xl overflow-x-scroll">
+            <button
+              onClick={() => window.location.reload()}
+              className="btn btn-primary mb-4"
+            >
+              Reload Page
+            </button>
+            <h1>Something went wrong.</h1>
+            <p className="break-all whitespace-pre font-mono text-sm">Error: {this.state.errorMessage}</p>
+            <pre className="text-xs mt-8 break-all whitespace-pre font-mono">{this.state.stack}</pre>
+          </div>
         </div>
       )
     }
