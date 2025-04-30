9c7b357a9486bcdad6d3a1ea81f571b6348abf3f
Martti Malmi <sirius@iki.fi>
2025-04-30 12:10:30 +0300

    direct nav to iris.to urls

--- src/shared/components/embed/Url.tsx
+++ src/shared/components/embed/Url.tsx
@@ -1,11 +1,23 @@
+import {Link} from "react-router"
 import Embed from "./index.ts"
 
 const Url: Embed = {
   regex: /(https?:\/\/[^\s,\\.]+(?:\.[^\s,.]+)*)/g,
   component: ({match}) => {
+    const url = new URL(match)
+    const displayText = match.replace(/^https?:\/\//, "").replace(/\/$/, "")
+
+    if (url.hostname === "iris.to") {
+      return (
+        <Link className="link link-info" to={url.pathname + url.search + url.hash}>
+          {displayText}
+        </Link>
+      )
+    }
+
     return (
       <a className="link link-info" target="_blank" href={match} rel="noreferrer">
-        {match.replace(/^https?:\/\//, "").replace(/\/$/, "")}
+        {displayText}
       </a>
     )
   },
