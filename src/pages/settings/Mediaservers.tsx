5469b0fd34fc5da9d5e1f1d345cc6fd366f141c8
Martti Malmi <sirius@iki.fi>
2025-07-16 11:02:16 +0300

    media servers

--- src/pages/settings/Mediaservers.tsx
+++ src/pages/settings/Mediaservers.tsx
@@ -2,31 +2,39 @@ import {useSubscriptionStatus} from "@/shared/hooks/useSubscriptionStatus"
 import {ChangeEvent, useState, useEffect} from "react"
 import {useUserStore} from "@/stores/user"
 
-const BLOSSOM_IRIS_TO = "https://blossom.iris.to"
+const MEDIASERVERS = {
+  iris: {
+    url: "https://blossom.iris.to",
+    protocol: "blossom" as const,
+  },
+  f7z: {
+    url: "https://blossom.f7z.io",
+    protocol: "blossom" as const,
+  },
+  nostr_build: {
+    url: "https://nostr.build/api/v2/nip96/upload",
+    protocol: "nip96" as const,
+  },
+  nostr_check: {
+    url: "https://cdn.nostrcheck.me",
+    protocol: "nip96" as const,
+  },
+}
 
 function getDefaultServers(isSubscriber: boolean) {
-  const defaults: {url: string; protocol: "blossom" | "nip96"; isDefault: boolean}[] =
-    isSubscriber
-      ? [
-          {
-            url: BLOSSOM_IRIS_TO,
-            protocol: "blossom",
-            isDefault: true,
-          },
-        ]
-      : [
-          {
-            url: "https://nostr.build/api/v2/nip96/upload",
-            protocol: "nip96",
-            isDefault: true,
-          },
-        ]
-  defaults.push({
-    url: "https://cdn.nostrcheck.me",
-    protocol: "nip96",
-    isDefault: true,
-  })
-  return defaults
+  return isSubscriber
+    ? [
+        MEDIASERVERS.iris,
+        MEDIASERVERS.f7z,
+        MEDIASERVERS.nostr_build,
+        MEDIASERVERS.nostr_check,
+      ]
+    : [
+        MEDIASERVERS.f7z,
+        MEDIASERVERS.nostr_build,
+        MEDIASERVERS.nostr_check,
+        MEDIASERVERS.iris,
+      ]
 }
 
 function stripHttps(url: string) {
