bfdf864ef9217e755390015124c5ea8f583371f9
laurikjk <kajakko.lauri@gmail.com>
2025-07-28 17:47:53 +0300

    SpecialFeed component and hook

---
+++ src/shared/hooks/usePostFetcher.ts
@@ -0,0 +1,69 @@
+import {useState, useEffect, useRef} from "react"
+import {NDKEvent, NDKFilter} from "@nostr-dev-kit/ndk"
+import {ndk} from "@/utils/ndk"
+
+interface PostFetcherCache {
+  events?: NDKEvent[]
+  hasLoadedInitial?: boolean
+}
+
+export default function usePostFetcher(
+  nextMostPopular: (n: number) => string[],
+  hasInitialData: boolean,
+  cache: PostFetcherCache
+) {
+  const [events, setEvents] = useState<NDKEvent[]>(cache.events || [])
+  const [loading, setLoading] = useState<boolean>(false)
+  const hasLoadedInitial = useRef(cache.hasLoadedInitial || false)
+
+  // Update cache when events change
+  useEffect(() => {
+    cache.events = events
+  }, [events])
+
+  // Update cache when hasLoadedInitial changes
+  useEffect(() => {
+    cache.hasLoadedInitial = hasLoadedInitial.current
+  }, [hasLoadedInitial.current])
+
+  const loadInitial = async () => {
+    setLoading(true)
+    const nextMostPopularEventIds = nextMostPopular(10)
+    if (nextMostPopularEventIds.length === 0) {
+      setLoading(false)
+      return
+    }
+    const postFilter: NDKFilter = {
+      kinds: [1],
+      ids: nextMostPopularEventIds,
+    }
+    const fetchedEvents = await ndk().fetchEvents(postFilter)
+    setEvents(Array.from(fetchedEvents))
+    setLoading(false)
+  }
+
+  const loadMore = async () => {
+    setLoading(true)
+    const nextMostPopularEventIds = nextMostPopular(10)
+    const postFilter: NDKFilter = {
+      kinds: [1],
+      ids: nextMostPopularEventIds,
+    }
+    const fetchedEvents = await ndk().fetchEvents(postFilter)
+    setEvents((prevEvents) => [...prevEvents, ...Array.from(fetchedEvents)])
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (hasInitialData && !hasLoadedInitial.current) {
+      hasLoadedInitial.current = true
+      loadInitial()
+    }
+  }, [hasInitialData])
+
+  return {
+    events,
+    loading,
+    loadMore,
+  }
+}
