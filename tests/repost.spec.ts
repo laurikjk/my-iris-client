4f2ca8389aa3da55c2e3cd0b17176fe8feca962a
laurikjk <kajakko.lauri@gmail.com>
2025-08-19 20:34:18 +0300

    Fix test that assumes home page has latest

--- tests/repost.spec.ts
+++ tests/repost.spec.ts
@@ -6,11 +6,11 @@ test("user can create a post", async ({page}) => {
 
   await page.locator("#main-content").getByTestId("new-post-button").click()
   const postContent = "Test post for basic functionality"
-  await page.getByRole("dialog").getByPlaceholder("What's on your mind?").fill(postContent)
+  await page
+    .getByRole("dialog")
+    .getByPlaceholder("What's on your mind?")
+    .fill(postContent)
   await page.getByRole("dialog").getByRole("button", {name: "Post"}).click()
 
-  await expect(page.getByText(postContent).first()).toBeVisible()
-
-  await page.getByRole("link", {name: "Home", exact: true}).click()
   await expect(page.getByText(postContent).first()).toBeVisible()
 })
