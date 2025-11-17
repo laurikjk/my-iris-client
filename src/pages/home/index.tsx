08aff4139d6e799eede64731bc92c985c22b1e3e
Martti Malmi <sirius@iki.fi>
2025-11-17 20:18:50 +0200

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -110,28 +110,7 @@ function Index() {
   return (
     <>
       <Header showBack={false} showNotifications={true}>
-        <div className="flex items-center justify-between w-full">
-          <span className="px-3 py-2">{feedName}</span>
-          {isLargeScreen && (
-            <button
-              className="p-2 bg-base-100 hover:bg-base-200 rounded-full transition-colors mt-1"
-              onClick={() =>
-                updateAppearance({singleColumnLayout: !appearance.singleColumnLayout})
-              }
-              title={
-                appearance.singleColumnLayout
-                  ? "Expand to two columns"
-                  : "Collapse to single column"
-              }
-            >
-              {appearance.singleColumnLayout ? (
-                <RiArrowLeftSLine className="w-5 h-5" />
-              ) : (
-                <RiArrowRightSLine className="w-5 h-5" />
-              )}
-            </button>
-          )}
-        </div>
+        <span className="px-3 py-2">{feedName}</span>
       </Header>
       <section
         className="flex w-full justify-center h-full overflow-hidden"
